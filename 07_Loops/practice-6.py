84a74ea7321f3c62b11b10cfb180c1cdc6185652
Mahak Naithani <naithanimahak@gmail.com>
2025-08-05 21:26:10 +0530

    Initial commit with clean codes

--- 07_Loops/practice-6.py
+++ 07_Loops/practice-6.py
@@ -7,5 +7,5 @@
 n = int(input("Enter number: "))
 for i in range(1, n+1):
     print(" " * (n-i),end="")
-    print("* "* i,end="")
+    print("* "* i,end="")#end is used so print statement doesn't actually creates a new line
     print("\n")
